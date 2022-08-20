01a42453d71a367f76b6ee6200695249c8ba8afd
Payam Qorbanpour <payam@Payams-MacBook-Air.local>
2022-08-20 19:18:52 +0430

    Init

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	err := router.Run(":" + port)
+	if err != nil {
+		log.Fatal(fmt.Errorf("error on run gin router: %w", err))
+	}
+}
